ceae5075ff7bb042c78ab8a154a3aeaf8e969885
Natalia-Nowak <nowak.natalia.080@gmail.com>
2023-09-17 22:34:43 +0200

    added async await

--- src/api-pixabay.js
+++ src/api-pixabay.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiKey = '39499742-e6ee54a981069064b6b4afa72';
 
-export const searchPics = (text, page) => {
+export const searchPics = async (text, page) => {
   const url = `https://pixabay.com/api/?key=${apiKey}&q=${text}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
-  return axios.get(url);
+  return await axios.get(url);
 };
